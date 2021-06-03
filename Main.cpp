08d9e2ea9dc2e329735f12543d2055e6ef450c8b
saivarshith2000 <hosvarshith@gmail.com>
2021-06-03 16:34:30 +0530

    Another commit

--- Main.cpp
+++ Main.cpp
@@ -36,10 +36,8 @@ public:
 
 class StudentRepository
 {
-private:
-	vector<Student> students;
-
 public:
+	vector<Student> students;
 	void AddStudent(Student student)
 	{
 		students.push_back(student);
@@ -48,10 +46,8 @@ public:
 
 class BookRepository
 {
-private:
-	vector<Book> books;
-
 public:
+	vector<Book> books;
 	void AddBook(Book book)
 	{
 		books.push_back(book);
@@ -105,8 +101,49 @@ public:
 	}
 };
 
+void BookRepoTest() {
+	// test if book is added to repo
+	BookRepository br;
+	Book b("A Book", 1);
+	br.AddBook(b);
+	assert(br.books.size() == 1);
+	assert(br.books[0].Name == "A Book");
+	assert(br.books[0].bookId == 1);
+
+	assert(br.FindBook("A Book")->Name == b.Name);
+}
+
+void StudentRepoTest() {
+	StudentRepository sr;
+	Student s("Test User", 1);
+	sr.AddStudent(s);
+	assert(sr.students.size() == 1);
+	assert(sr.students[0].Name == "Test User");
+	assert(sr.students[0].Id == 1);
+}
+
+void LibraryTest() {
+	StudentRepository sr;
+	BookRepository br;
+	Student s("Temp Student", 1);
+	Library lb(sr, br);
+	// test for empty books
+	assert(lb.BorrowBook("Any Name", s)->bookId == -1);
+}
+
+void TESTS() {
+	BookRepoTest();
+	StudentRepoTest();
+	LibraryTest();
+}
+
 int main()
 {
+	// Unit tests
+	TESTS();
+	cout << "ALL UNIT TESTS PASSED!\n" << endl;
+
+	// integration tests
 	StudentRepository studentRepo;
 	BookRepository bookRepo;
 	// Add users to student repository
@@ -125,5 +162,7 @@ int main()
 	bookRepo.AddBook(b3);
 	Library lib(studentRepo, bookRepo);
 	Book* b = lib.BorrowBook("A Book", s1);
-	cout << b1.Name << endl;
+	cout << b->Name << endl;
+	Book* bb = lib.BorrowBook("A Book doesn't exist", s1);
+	cout << bb->Name << endl;
 }
